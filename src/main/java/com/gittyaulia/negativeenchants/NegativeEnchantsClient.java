ca3300770952a2f176a7f1cf6bd312146e65ce24
Andini Faramita <faramitaandini@gmail.com>
2025-03-21 00:52:43 +0100

    Initial commit

---
+++ src/main/java/com/gittyaulia/negativeenchants/NegativeEnchantsClient.java
@@ -0,0 +1,10 @@
+package com.gittyaulia.negativeenchants;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NegativeEnchantsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/gittyaulia/negativeenchants/NegativeEnchantsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.gittyaulia.negativeenchants;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NegativeEnchantsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
